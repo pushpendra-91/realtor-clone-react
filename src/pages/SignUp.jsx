57027aa13ea8c1099a5c30f25b389a6ae82b4198
Pushpendra Kanaujiya <pkanaujiya91@gmail.com>
2023-10-01 17:26:23 +0530

    integrated firebase authentication

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,8 +2,14 @@ import React, { useState } from 'react';
 import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth'
+import { db } from '../firebase';
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function SignUp() {
+  const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     name: '',
@@ -17,6 +23,28 @@ export default function SignUp() {
       [e.target.id]: e.target.value,
     }));
   }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+      const user = userCredential.user
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
+      // toast.success("SignUp Success!");
+      navigate("/");
+      console.log("user: ", user)
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  }
+
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign Up</h1>
@@ -26,7 +54,7 @@ export default function SignUp() {
         </div>
 
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="text" id="name" value={name} onChange={onChange} placeholder="Full Name" className="mb-6 w-full px-4 py-2 text-xl bg-white text-gray-700 rounded border-gray-300" />
 
             <input type="email" id="email" value={email} onChange={onChange} placeholder="Email Address" className="mb-6 w-full px-4 py-2 text-xl bg-white text-gray-700 rounded border-gray-300" />
@@ -44,7 +72,7 @@ export default function SignUp() {
               <p className='mb-6'>Have an account? <Link to="/sign-in" className='text-red-500 hover:text-red-700 ml-1'>Sign Up</Link></p>
               <p><Link to="/forgot-password" className='text-blue-500 hover:text-blue-600'>Forgot Password?</Link></p>
             </div>
-            <button className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-sm hover:bg-blue-700 hover:shadow-lg active:bg-blue-900' type='submit'>Sign In</button>
+            <button className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-sm hover:bg-blue-700 hover:shadow-lg active:bg-blue-900' type='submit'>Sign Up</button>
             <div className='flex items-center my-4 before:border-t before:flex-1  before:before-gray-300 after:border-t after:flex-1 after:border-gray-300'>
               <p className='text-center font-semibold mx-4'>OR
               </p>
