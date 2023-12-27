28002a6dd8c79a3ce9ce827d777b087d85531d5d
Pushpendra Kanaujiya <pkanaujiya91@gmail.com>
2023-12-27 22:44:34 +0530

    added onChange functionlity to create listing page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
 import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { getAuth } from 'firebase/auth';
+import { v4 as uuidv4 } from 'uuid';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useNavigate } from 'react-router';
+
 
 export default function CreateListing() {
-    const [geoLocationEnabled, setGeoLocationEnabled] = useState(true);
+    const auth = getAuth();
+    const navigate = useNavigate();
+    const [geoLocationEnabled, setGeoLocationEnabled] = useState(false);
     const [loading, setLoading] = useState(false);
     const [formData, setFormData] = useState({
         type: "rent",
@@ -49,10 +58,11 @@ export default function CreateListing() {
         }
     }
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
-        if (discountedPrice >= regularPrice) {
+        if (parseInt(discountedPrice, 10) >= parseInt(regularPrice, 10)) {
+            console.log(typeof discountedPrice);
             setLoading(false);
             toast.error("Discounted Price should be less than Regular Price");
             return;
@@ -66,8 +76,81 @@ export default function CreateListing() {
         let geoLocation = {}
         let location
         if (geoLocationEnabled) {
+            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_geocode_apiKey}`);
+            const data = await response.json();
+            geoLocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+            geoLocation.lng = data.results[0]?.geometry.location.lng ?? 0;
 
+            location = data.status === "ZERO_RESULTS" && undefined;
+
+            if (location === undefined || location.includes("undefined")) {
+                setLoading(false);
+                toast.error("Enter a correct Address");
+                return;
+            }
+        } else {
+            geoLocation.lat = latitude;
+            geoLocation.lng = longitude;
         }
+
+        async function storeImage(image) {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage()
+                const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+                const storageRef = ref(storage, filename);
+                const uploadTask = uploadBytesResumable(storageRef, image);
+                uploadTask.on('state_changed',
+                    (snapshot) => {
+                        // Observe state change events such as progress, pause, and resume
+                        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                            case 'paused':
+                                console.log('Upload is paused');
+                                break;
+                            case 'running':
+                                console.log('Upload is running');
+                                break;
+                        }
+                    },
+                    (error) => {
+                        // Handle unsuccessful uploads
+                        reject(error)
+                    },
+                    () => {
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                            // console.log('File available at', downloadURL);
+                            resolve(downloadURL);
+                        });
+                    }
+                );
+
+            })
+        }
+
+        const imgUrls = await Promise.all([...images].map((image) => storeImage(image))).catch((error) => {
+            setLoading(false);
+            toast.error("Images not uploaded");
+            return;
+        })
+        // console.log("urls: ", imgUrls);
+        const formDataCopy = {
+            ...formData,
+            imgUrls,
+            geoLocation,
+            timestamp: serverTimestamp(),
+        };
+        delete formDataCopy.images;
+        !formDataCopy.offer && delete formDataCopy.discountedPrice;
+        delete formDataCopy.latitude;
+        delete formDataCopy.longitude;
+        const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
+        setLoading(false);
+        toast.success("Listing Created");
+        navigate(`/category/${formDataCopy.type}/${docRef.id}`);
     }
 
     if (loading) {
