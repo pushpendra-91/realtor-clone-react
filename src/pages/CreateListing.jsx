7eacc9a1694a7ae8da58e91e5c508ea762d12513
Pushpendra Kanaujiya <pkanaujiya91@gmail.com>
2023-12-27 21:18:51 +0530

    update create-listing page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
+import Spinner from '../components/Spinner';
+import { toast } from 'react-toastify'
 
 export default function CreateListing() {
+    const [geoLocationEnabled, setGeoLocationEnabled] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [formData, setFormData] = useState({
         type: "rent",
-        name: "",
+        name: '',
         bedrooms: 1,
         bathrooms: 1,
         parking: false,
@@ -13,22 +17,71 @@ export default function CreateListing() {
         offer: false,
         regularPrice: 0,
         discountedPrice: 0,
+        latitude: 0,
+        longitude: 0,
+        images: {},
     });
 
-    const { type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice } = formData;
+    const { type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice, latitude, longitude, images } = formData;
 
-    const onChange = () => {
+    const onChange = (e) => {
+        let boolean = null;
+        if (e.target.value === "true")
+            boolean = true;
 
+        if (e.target.value === "false")
+            boolean = false;
+
+        //Image Files
+        if (e.target.files) {
+            setFormData((prevState) => ({
+                ...prevState,
+                images: e.target.files,
+            }))
+        }
+
+        // Text/Boolean/Number
+        if (!e.target.files) {
+            setFormData((prevState) => ({
+                ...prevState,
+                [e.target.id]: boolean ?? e.target.value,
+            }))
+        }
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        if (discountedPrice >= regularPrice) {
+            setLoading(false);
+            toast.error("Discounted Price should be less than Regular Price");
+            return;
+        }
+        if (images.length > 6) {
+            setLoading(false);
+            toast.error("Maximum 6 images are allowed");
+            return;
+        }
+
+        let geoLocation = {}
+        let location
+        if (geoLocationEnabled) {
+
+        }
+    }
+
+    if (loading) {
+        return <Spinner />;
     }
     return (
         <main className='max-w-md px-2 mx-auto'>
             <h1 className='text-3xl text-center mt-6 font-bold'>Create a Listing</h1>
-            <form>
+            <form onSubmit={onSubmit}>
                 <p className='text-lg mt-6 font-semibold'>Sell / Rent</p>
                 <div className='flex '>
-                    <button type='button' id='type' value='sale' onClick={onchange} className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${type === 'rent' ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Sell</button>
+                    <button type='button' id='type' value='sale' onClick={onChange} className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${type === 'rent' ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Sell</button>
 
-                    <button type='button' id='type' value='sale' onClick={onchange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${type === 'sell' ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Rent</button>
+                    <button type='button' id='type' value='rent' onClick={onChange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${type === 'sale' ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Rent</button>
                 </div>
                 <p className='text-lg mt-6 font-semibold'>Name</p>
                 <input type='text' id='name' value={name} onChange={onChange} placeholder='Name' maxLength='32' minLength='4' required className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6' />
@@ -44,27 +97,38 @@ export default function CreateListing() {
                 </div>
                 <p className='text-lg mt-6 font-semibold'>Parking Spot</p>
                 <div className='flex '>
-                    <button type='button' id='parking' value={true} onClick={onchange} className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${!parking ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Yes</button>
+                    <button type='button' id='parking' value={true} onClick={onChange} className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${!parking ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Yes</button>
 
-                    <button type='button' id='parking' value={false} onClick={onchange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${parking ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>No</button>
+                    <button type='button' id='parking' value={false} onClick={onChange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${parking ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>No</button>
                 </div>
                 <p className='text-lg mt-6 font-semibold'>Furnished</p>
                 <div className='flex '>
-                    <button type='button' id='furnished' value={true} onClick={onchange} className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${!furnished ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Sell</button>
+                    <button type='button' id='furnished' value={true} onClick={onChange} className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${!furnished ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Sell</button>
 
-                    <button type='button' id='furnished' value={false} onClick={onchange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${furnished ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Rent</button>
+                    <button type='button' id='furnished' value={false} onClick={onChange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${furnished ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Rent</button>
                 </div>
                 <p className='text-lg mt-6 font-semibold'>Address</p>
                 <textarea type='text' id='address' value={address} onChange={onChange} placeholder='Address' required className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6' />
-
+                {!geoLocationEnabled && (
+                    <div className='flex space-x-6 justify-start mb-6'>
+                        <div>
+                            <p className='text-lg font-semibold'>Latitude</p>
+                            <input type='number' id='latitude' value={latitude} onChange={onChange} min='-90' max='90' required className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6 text-center' />
+                        </div>
+                        <div>
+                            <p className='text-lg font-semibold'>Longitude</p>
+                            <input type='number' id='longitude' value={longitude} onChange={onChange} min='-180' max='180' required className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6 text-center' />
+                        </div>
+                    </div>
+                )}
                 <p className='text-lg font-semibold'>Description</p>
                 <textarea type='text' id='description' value={description} onChange={onChange} placeholder='Description' required className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6' />
 
                 <p className='text-lg font-semibold'>Offer</p>
                 <div className='flex mb-6'>
-                    <button type='button' id='offer' value={true} onClick={onchange} className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${!offer ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Yes</button>
+                    <button type='button' id='offer' value={true} onClick={onChange} className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${!offer ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>Yes</button>
 
-                    <button type='button' id='offer' value={false} onClick={onchange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${offer ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>No</button>
+                    <button type='button' id='offer' value={false} onClick={onChange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${offer ? 'bg-white text-black' : 'bg-slate-600 text-white'}`}>No</button>
                 </div>
                 <div className='flex items-center mb-6'>
                     <div>
