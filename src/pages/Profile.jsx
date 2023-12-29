857e198990d8c67ef854e0a6467127f18f7ecc2b
Pushpendra Kanaujiya <pkanaujiya91@gmail.com>
2023-12-29 19:59:50 +0530

    added edit and delete functionality to listings

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { getAuth, updateProfile } from 'firebase/auth'
-import { collection, doc, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -68,6 +68,20 @@ export default function Profile() {
     fetchUserListings();
   }, [auth.currentUser.uid]);
 
+  const onDelete = async (listingId) => {
+    if (window.confirm("Are you sure, to delete this item?")) {
+      await deleteDoc(doc(db, 'listings', listingId));
+      const updatedListings = listings.filter((listing) => listing.id !== listingId);
+      setListings(updatedListings);
+      toast.success("Delete successfully!")
+    }
+  }
+
+  const onEdit = (listingId) => {
+    navigate(`/edit-listing/${listingId}`);
+  }
+
+
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -95,10 +109,11 @@ export default function Profile() {
       <div className='max-w-6xl px-3 mt-6 mx-auto'>
         {!loading && listings.length > 0 && (
           <>
-            <h2 className='text-2xl text-center font-semibold'>My Listings</h2>
-            <ul>
+            <h2 className='text-2xl text-center font-semibold mb-6'>My Listings</h2>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6'>
               {listings.map((listing) => (
-                <ListingItem key={listing.id} id={listing.id} listing={listing.data} />
+                <ListingItem key={listing.id} id={listing.id} listing={listing.data} onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)} />
               ))}
             </ul>
           </>
