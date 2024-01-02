01b92819b7882524a31214fcf59dbf438c6cde67
Pushpendra Kanaujiya <pkanaujiya91@gmail.com>
2024-01-02 18:45:19 +0530

    fixed leaflet (map) popup

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -108,8 +108,7 @@ export default function Listing() {
                     {contactLandLord && (
                         <Contact userRef={listing.userRef} listing={listing} />
                     )}
-                </div>
-                <div className='w-full h-[200px] lg:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2'>
+                </div>                <div className='w-full h-[200px] lg:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2'>
                     <MapContainer center={[listing.geoLocation.lat, listing.geoLocation.lng]} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
                         <TileLayer
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -117,7 +116,7 @@ export default function Listing() {
                         />
                         <Marker position={[listing.geoLocation.lat, listing.geoLocation.lng]}>
                             <Popup>
-                                A pretty CSS3 popup. <br /> Easily customizable.
+                                {listing.address}
                             </Popup>
                         </Marker>
                     </MapContainer>
